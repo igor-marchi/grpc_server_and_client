6126c8c0f1ec240b5142abe54e5926129e6478fd
igor-marchi <igoraugustomarchi@gmail.com>
2022-03-30 23:17:03 -0300

    create server and client gRPC

---
+++ cmd/client/client.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/igor-marchi/grpc_full_cycle/pb"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	connection, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Could not connect to gRPC Server: %v", err)
+	}
+	defer connection.Close()
+
+	client := pb.NewUserServiceClient(connection)
+	addUser(client)
+}
+
+func addUser(client pb.UserServiceClient) {
+	req := &pb.User{
+		Id:    "0",
+		Name:  "João",
+		Email: "j@j.com",
+	}
+
+	res, err := client.AddUser(context.Background(), req)
+	if err != nil {
+		log.Fatalf("Could not make gRPC request: %v", err)
+	}
+
+	fmt.Println(res)
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/igor-marchi/grpc_full_cycle
+
+go 1.14
+
+require (
+	google.golang.org/grpc v1.45.0
+	google.golang.org/protobuf v1.28.0
+)
